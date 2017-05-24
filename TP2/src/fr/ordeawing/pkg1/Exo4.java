b9dc6f084c1a24582cc1405d4df227296d500bf8
Ordeawing-Linux <romain88130@gmail.com>
2017-05-24 10:52:07 +0200

    Add some shit to Exo4

--- TP2/src/fr/ordeawing/pkg1/Exo4.java
+++ TP2/src/fr/ordeawing/pkg1/Exo4.java
@@ -1,6 +1,8 @@
 package fr.ordeawing.pkg1;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -14,5 +16,13 @@ public class Exo4 {
         DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("EEEE dd MMM yyyy HH:mm");
         String format1 = time1.format(formatter1);
         System.out.println(format1);
+
+        LocalDateTime nowPlus = time1.plusHours(2).plusMinutes(45);
+        System.out.println(nowPlus.format(formatter1));
+        LocalDate birthday = LocalDate.of(2002,8,18);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy");
+        System.out.println(birthday.format(dateFormatter));
+        Period period = Period.between(birthday, time1.toLocalDate());
+        System.out.println("J'suis agé de " + period.getYears() + "ans " + period.getMonths() + "mois " + period.getDays() + "jours.");
     }
 }
